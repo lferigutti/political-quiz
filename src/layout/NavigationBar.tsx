03038442d2ffdc8476c79f943c119c6e29ca92cd
Leonardo <leonardoferigutti@gmail.com>
2025-03-23 13:50:52 +0100

    nvalink

--- src/layout/NavigationBar.tsx
+++ src/layout/NavigationBar.tsx
@@ -1,22 +1,25 @@
 import { NavLink } from "react-router";
 
 const NavigationBar = () => {
-  const styleActive = "font-semibold";
 
   return (
     <nav className="gap-5 flex items-center">
-      <NavLink to="/" className={({ isActive }) => isActive && styleActive} end>
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
+      >
         Home
       </NavLink>
-      {/*<Button color="default" variant="outlined">*/}
       <NavLink
         to="/test-politico"
-        className={({ isActive }) => isActive && styleActive}
-        end
+        style={({ isActive }) => ({
+          fontWeight: isActive ? "bold" : "normal",
+        })}
       >
         Test Politico
       </NavLink>
-      {/*</Button>*/}
     </nav>
   );
 };
